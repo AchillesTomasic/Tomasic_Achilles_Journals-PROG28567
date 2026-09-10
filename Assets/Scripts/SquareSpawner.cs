856c20fe645b44b29fa588b301040abbdd6e1b88
ACHILLESLAPTOP\tomas <tomasiac@sheridancollege.ca>
2026-09-09 20:29:41 -0400

    Add project files.

---
+++ Assets/Scripts/SquareSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0;
+        float y = 0;
+    }
+}
